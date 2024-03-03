9a7f92ae110b5f3f67195d48c20afafebd027bf9
MMJeison <jeison.moncayo@udea.edu.co>
2024-03-03 00:23:01 -0500

    Generated basic structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./auth/auth.route').then(m => m.AUTH_ROUTES)
+  },
+];
---
+++ src/app/shared/directivas/switch/switch.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switch',
+  standalone: true,
+  imports: [],
+  templateUrl: './switch.component.html'
+})
+export class SwitchComponent {
+
+}
